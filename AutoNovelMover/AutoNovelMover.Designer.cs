c0f408b3e7c6595e28e9f32225a1bbec60dcd29a
Youn Jong Kyun <dbswhdrbs@naver.com>
2014-01-12 20:44:02 +0900

    AutoNovelMover v1.0

---
+++ AutoNovelMover/AutoNovelMover.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace AutoNovelMover
+{
+    partial class AutoNovelMover
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoNovelMover));
+            this.NovelListView = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.targetDir = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.Button();
+            this.AutoCopyStart = new System.Windows.Forms.Button();
+            this.copyProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // NovelListView
+            // 
+            this.NovelListView.AllowDrop = true;
+            this.NovelListView.Location = new System.Drawing.Point(0, 39);
+            this.NovelListView.Name = "NovelListView";
+            this.NovelListView.Size = new System.Drawing.Size(468, 234);
+            this.NovelListView.TabIndex = 0;
+            this.NovelListView.UseCompatibleStateImageBehavior = false;
+            this.NovelListView.DragDrop += new System.Windows.Forms.DragEventHandler(this.NovelListView_DragDrop);
+            this.NovelListView.DragOver += new System.Windows.Forms.DragEventHandler(this.NovelListView_DragOver);
+            this.NovelListView.QueryContinueDrag += new System.Windows.Forms.QueryContinueDragEventHandler(this.NovelListView_QueryContinueDrag);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "복사 타겟 디렉토리 경로 :";
+            // 
+            // targetDir
+            // 
+            this.targetDir.Location = new System.Drawing.Point(163, 12);
+            this.targetDir.Name = "targetDir";
+            this.targetDir.Size = new System.Drawing.Size(216, 21);
+            this.targetDir.TabIndex = 2;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Location = new System.Drawing.Point(389, 13);
+            this.openFileDialog.Name = "openFileDialog";
+            this.openFileDialog.Size = new System.Drawing.Size(66, 19);
+            this.openFileDialog.TabIndex = 3;
+            this.openFileDialog.Text = "폴더 찾기";
+            this.openFileDialog.UseVisualStyleBackColor = true;
+            this.openFileDialog.Click += new System.EventHandler(this.openFileDialog_Click);
+            // 
+            // AutoCopyStart
+            // 
+            this.AutoCopyStart.Location = new System.Drawing.Point(-1, 305);
+            this.AutoCopyStart.Name = "AutoCopyStart";
+            this.AutoCopyStart.Size = new System.Drawing.Size(469, 34);
+            this.AutoCopyStart.TabIndex = 4;
+            this.AutoCopyStart.Text = "자동 복사 시작";
+            this.AutoCopyStart.UseVisualStyleBackColor = true;
+            this.AutoCopyStart.Click += new System.EventHandler(this.AutoCopyStart_Click);
+            // 
+            // copyProgressBar
+            // 
+            this.copyProgressBar.Location = new System.Drawing.Point(0, 277);
+            this.copyProgressBar.Name = "copyProgressBar";
+            this.copyProgressBar.Size = new System.Drawing.Size(468, 22);
+            this.copyProgressBar.TabIndex = 5;
+            // 
+            // AutoNovelMover
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(468, 338);
+            this.Controls.Add(this.copyProgressBar);
+            this.Controls.Add(this.AutoCopyStart);
+            this.Controls.Add(this.openFileDialog);
+            this.Controls.Add(this.targetDir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NovelListView);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AutoNovelMover";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoNovelMover v1.0";
+            this.Load += new System.EventHandler(this.AutoNovelMover_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView NovelListView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox targetDir;
+        private System.Windows.Forms.Button openFileDialog;
+        private System.Windows.Forms.Button AutoCopyStart;
+        private System.Windows.Forms.ProgressBar copyProgressBar;
+    }
+}
+
---
+++ AutoNovelMover/AutoNovelMover.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AutoNovelMover
+{
+    public partial class AutoNovelMover : Form
+    {
+        /// <summary>
+        /// 소설의 파일정보를 저장해 놓는다.
+        /// </summary>
+        protected Dictionary<string, FileInfo> novelFileInfos = new Dictionary<string, FileInfo>();
+        // 진행바 값을 보내기 위한 콜백
+        public delegate void SetProgCallBack(int value);
+        // 쓰레드
+        private System.Threading.Thread copyThread;
+        // 복사중인지 체크
+        private bool working { get; set; }
+
+        public AutoNovelMover()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 폼 로드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AutoNovelMover_Load(object sender, EventArgs e)
+        {
+            // 뷰모드 - 디테일
+            NovelListView.View = View.Details;
+            NovelListView.GridLines = true;
+
+            NovelListView.Columns.Add("번호", 40, HorizontalAlignment.Right);
+            NovelListView.Columns.Add("파일명", 180, HorizontalAlignment.Left);
+            NovelListView.Columns.Add("사이즈", 70, HorizontalAlignment.Right);
+            NovelListView.Columns.Add("날짜", 170, HorizontalAlignment.Left);
+
+            working = false;
+        }
+
+        /// <summary>
+        /// 리스트뷰에 드래그 앤 드롭을 했을때, 리스트뷰로 구성해준다.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NovelListView_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+                {
+                    NovelListView.BeginUpdate();
+
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    foreach (string fileName in files)
+                    {
+                        // 파일의 정보를 읽어온다.
+                        FileInfo fileInfo = new FileInfo(fileName);
+                        if (novelFileInfos.ContainsKey(fileName) == false)
+                        {
+                            // 새로운 리스트 아이템 구성
+                            ListViewItem newItem = new ListViewItem((novelFileInfos.Count + 1).ToString());
+                            newItem.SubItems.Add(fileInfo.Name);
+                            newItem.SubItems.Add(GetFileSize(fileInfo.Length));
+                            newItem.SubItems.Add(fileInfo.LastWriteTime.ToString());
+                            NovelListView.Items.Add(newItem);
+
+                            novelFileInfos.Add(fileName, fileInfo);
+                        }
+                        else
+                        {
+                            Console.WriteLine("같은파일을 중복해서 넣으려 하였습니다.");
+                        }
+                    }
+
+                    NovelListView.EndUpdate();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "리스트 구성 에러");
+            }
+        }
+
+        private void NovelListView_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+            if (e.EscapePressed)
+            {
+                e.Action = DragAction.Cancel;
+            }
+        }
+
+        private void NovelListView_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private string GetFileSize(double byteCount)
+        {
+            string size = "0 Bytes";
+            if (byteCount >= 1073741824.0)
+                size = String.Format("{0:##.##}", byteCount / 1073741824.0) + " GB";
+            else if (byteCount >= 1048576.0)
+                size = String.Format("{0:##.##}", byteCount / 1048576.0) + " MB";
+            else if (byteCount >= 1024.0)
+                size = String.Format("{0:##.##}", byteCount / 1024.0) + " KB";
+            else if (byteCount > 0 && byteCount < 1024.0)
+                size = byteCount.ToString() + " Bytes";
+
+            return size;
+        }
+
+        /// <summary>
+        /// 소설을 복사할 폴더를 선택가능한 다이얼로그 박스를 연다.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openFileDialog_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            targetDir.Text = dialog.SelectedPath;
+        }
+
+        private void AutoCopyStart_Click(object sender, EventArgs e)
+        {
+            // 넣을 소설이 없거나, 타겟 디렉토리가 없다면 무시한다.
+            if (novelFileInfos.Count <= 0)
+            {
+                MessageBox.Show("자동 복사할 파일목록이 없습니다.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(targetDir.Text))
+            {
+                MessageBox.Show("타겟 폴더를 지정하지 않았습니다.");
+                return;
+            }
+
+            try
+            {
+                if (working) { return; }
+
+                // 타겟 디렉토리를 검색합니다.
+                DirectoryInfo dirInfo = new DirectoryInfo(targetDir.Text);
+                if (dirInfo.Exists)
+                {
+                    // 진행률의 최대값을 복사하려는 파일의 갯수로 지정
+                    copyProgressBar.Maximum = novelFileInfos.Count;
+                    // 복사 쓰레드 시작
+                    copyThread = new Thread(new ThreadStart(ProgressCopy));
+                    copyThread.Start();
+                    working = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                working = false;
+                MessageBox.Show(ex.Message, "자동 복사 에러");
+            }
+        }
+
+        /// <summary>
+        /// 복사 진행률을 표시한다.
+        /// </summary>
+        /// <param name="value"></param>
+        private void SetProgBar(int value)
+        {
+            if (copyProgressBar.InvokeRequired)
+            {
+                // 콜백 생성
+                SetProgCallBack callback = new SetProgCallBack(SetProgBar);
+                Invoke(callback, new object[] { value });
+            }
+            else
+            {
+                copyProgressBar.Value = value;
+            }
+        }
+
+        /// <summary>
+        /// 쓰레드로 파일복사 시작
+        /// </summary>
+        private void ProgressCopy()
+        {
+            // 타겟 디렉토리를 검색합니다.
+            DirectoryInfo dirInfo = new DirectoryInfo(targetDir.Text);
+            if (dirInfo.Exists)
+            {
+                int currentCopyCount = 1;
+                foreach (var file in novelFileInfos.Values)
+                {
+                    // 파일확장자를 제외한 파일이름만 추출
+                    string fileName = file.Name.Substring(0, file.Name.Length - file.Extension.Length);
+                    int lastIndex = fileName.LastIndexOf(" ");
+                    // 예외처리
+                    if (lastIndex == -1)
+                    {
+                        currentCopyCount++;
+                        continue;
+                    }
+
+                    fileName = fileName.Substring(0, fileName.LastIndexOf(" "));
+                    // 파일이름의 디렉토리 검색
+                    DirectoryInfo searchDir = new DirectoryInfo(string.Format("{0}\\{1}", dirInfo.FullName, fileName));
+                    if (searchDir.Exists == false)
+                    {
+                        // 폴더 생성
+                        searchDir.Create();
+                    }
+
+                    // 소설을 타겟폴더로 복사합니다.
+                    file.CopyTo(string.Format("{0}\\{1}", searchDir.FullName, file.Name), true);
+                    // 진행률 표시
+                    SetProgBar(currentCopyCount++);
+                }
+
+                working = false;
+                MessageBox.Show(string.Format("모든 소설을 [{0}] 폴더내에 각각 복사하였습니다.", dirInfo.FullName));
+            }
+        }
+    }
+}
